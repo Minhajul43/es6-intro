37b8963494b983731e27aea955fd2e41d8f1b730
Minhajul43 <minhajul255685islam@gmail.com>
2026-03-02 12:05:19 +0600

    keys-values

---
+++ keys-values.js
@@ -0,0 +1,8 @@
+const king = { name: 'Minhajul', age: 23, Kingdom: 'Dhaka' };
+const keys = Object.keys(king);
+const values = Object.values(king);
+const entrie = Object.entries(king);
+
+console.log(keys);
+console.log(values);
+console.log(entrie);
